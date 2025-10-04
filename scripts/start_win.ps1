3e0cc33333e31a36f5fe0efa1c0277949ca46ec5
AHY123 <andrewhyang@gmail.com>
2025-10-04 16:39:20 -0700

    error fix

--- scripts/start_win.ps1
+++ scripts/start_win.ps1
@@ -9,6 +9,5 @@ $env:MAMBA_ROOT_PREFIX = $MambaRoot
 
 $HostName = if ($env:HOST) { $env:HOST } else { "127.0.0.1" }
 $PortNum = if ($env:PORT) { $env:PORT } else { "8000" }
-Write-Host "Starting BLAST FastAPI at http://$HostName:$PortNum ..."
+Write-Host ("Starting BLAST FastAPI at http://{0}:{1} ..." -f $HostName, $PortNum)
 & $Micromamba run -p $EnvPrefix uvicorn backend.app.main:app --host $HostName --port $PortNum
-
